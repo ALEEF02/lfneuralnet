22faf7a9eddb32a62d9bbf9c779fd4ca90b84905
Tech-Person <joey1216@gmail.com>
2019-09-09 23:19:51 -0700

    Working prototype with testing data in 2 different formats

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+def sigmoid(x):
+	return 1.0/(1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+	return x * (1.0 - x)
+
 class NeuralNetwork:
 	def __init__(self, x, y):
 		self.input		= x
@@ -8,7 +16,7 @@ class NeuralNetwork:
 
 	def feedforward(self):
 		self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-		self.output = sigmoid(np.dot(drlg.layer1, self.weights2))
+		self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
 	def backprop(self):
 		d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
@@ -16,3 +24,19 @@ class NeuralNetwork:
 
 		self.weights1 += d_weights1
 		self.weights2 += d_weights2
+
+if __name__ == "__main__":
+	X = np.array([
+		[0,0,1],
+		[0,1,1],
+		[1,0,1],
+		[1,1,1]
+	])
+	y = np.array([[0],[1],[1],[0]])
+	nn = NeuralNetwork(X,y)
+
+	for i in range(1500):
+		nn.feedforward()
+		nn.backprop()
+
+	print(nn.output)
